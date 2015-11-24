18719153ead9256d22878dc6efe7355c32475a1d
Daniel Wiberg <daniel.wiberg@oryx.se>
2015-11-24 13:57:33 +0100

    Added native file dialog library

---
+++ src/3rdparty/nativefiledialog/nfd_win32.c
@@ -0,0 +1,30 @@
+
+#include "nfd_common.h"
+
+#include <wchar.h>
+#include <stdio.h>
+#include <assert.h>
+#include <windows.h>
+
+nfdresult_t NFD_OpenDialog( const nfdchar_t *filterList,
+                            const nfdchar_t *defaultPath,
+                            nfdchar_t **outPath )
+{
+  return NFD_ERROR;
+}
+
+/* multiple file open dialog */
+nfdresult_t NFD_OpenDialogMultiple( const nfdchar_t *filterList,
+                                    const nfdchar_t *defaultPath,
+                                    nfdpathset_t *outPaths )
+{
+  return NFD_ERROR;
+}
+
+/* save dialog */
+nfdresult_t NFD_SaveDialog( const nfdchar_t *filterList,
+                            const nfdchar_t *defaultPath,
+                            nfdchar_t **outPath )
+{
+  return NFD_ERROR;
+}
